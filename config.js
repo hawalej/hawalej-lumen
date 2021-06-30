cdfc01862a8eb79aed6bc8e8253e22ca11c04db3
Grzegorz Hawałej <34006095+hawalej@users.noreply.github.com>
2021-06-30 10:37:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ module.exports = {
     },
     {
       label: 'O mnie',
-      path: '/pages/about'
+      path: '/pages/omnie'
     },
     {
       label: 'Kontakt',
-      path: '/pages/contacts'
+      path: '/pages/kontakt'
     }
   ],
   author: {
